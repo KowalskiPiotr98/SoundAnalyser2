ab879dfda27fa78981c79f81427537a48fdee415
Piotr Kowalski <Piotr.Kowalski.1998@protonmail.com>
2020-05-08 15:28:42 +0200

    Implement the most basic features

---
+++ SoundAnalyser2/Parameters/FastFourierTransform.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoundAnalyser2.Parameters
+{
+    internal static class FastFourierTransform
+    {
+#pragma warning disable S125
+        //public enum WindowFunction
+        // Sections of code should not be commented out
+        //{
+        //    Rectangular,
+        //    Hamming,
+        //    Hann
+        //}
+#pragma warning restore S125 // Might be removed later
+        public static float [] FullFFT (float [] samples, int sampleRate)
+        {
+            if (samples is null)
+            {
+                throw new ArgumentNullException (nameof (samples));
+            }
+            return SelectedFrameFFT (samples, sampleRate, 0, samples.Length);
+        }
+
+        public static float [] SelectedFrameFFT (float [] samples, int sampleRate, int startFrame, int frameLength)
+        {
+            if (samples is null)
+            {
+                throw new ArgumentNullException (nameof (samples));
+            }
+            if (frameLength < 0 || frameLength > samples.Length)
+            {
+                frameLength = 256;
+            }
+            var tempFFT = new double [frameLength % 2 == 0 ? frameLength + 2 : frameLength + 1];
+            var startSample = startFrame * frameLength;
+            if (startSample < 0 || startSample > samples.Length)
+            {
+                startSample = 0;
+            }
+            if (startSample + frameLength > samples.Length)
+            {
+                frameLength = samples.Length - startSample;
+            }
+            Array.Copy (samples, startSample, tempFFT, 0, frameLength);
+            MathNet.Numerics.IntegralTransforms.Fourier.ForwardReal (/*ApplyWindowFunction*/ (tempFFT), frameLength, MathNet.Numerics.IntegralTransforms.FourierOptions.NoScaling);
+            var FFTD = new Dictionary<int, double> ();
+            for (int i = 0; i < tempFFT.Length; i += 2)
+            {
+                var x = (int)(1.0 * sampleRate / frameLength * (i / 2));
+                tempFFT [i] = Math.Sqrt (tempFFT [i] * tempFFT [i] + tempFFT [i + 1] * tempFFT [i + 1]);
+                if (FFTD.ContainsKey (x) && FFTD [x] < 20 * Math.Log10 (tempFFT [i]))
+                {
+                    FFTD [x] = 20 * Math.Log10 (tempFFT [i]);
+                }
+                else if (!FFTD.ContainsKey (x))
+                {
+                    FFTD.Add (x, 20 * Math.Log10 (tempFFT [i]));
+                }
+            }
+            var FFT = new float [FFTD.Keys.Max () + 1];
+            Parallel.ForEach (FFTD, (item) => FFT [item.Key] = (float)item.Value);
+
+            if (FFTD.Keys.Count != FFT.Length)
+            {
+                FFT = FillFFT (FFT, FFTD);
+            }
+            FFT [0] = 0;
+            return FFT;
+        }
+
+        private static float [] FillFFT (float [] FFT, Dictionary<int, double> FFTD)
+        {
+            _ = Parallel.For (0, FFT.Length, (i) =>
+            {
+                if (FFTD.ContainsKey (i))
+                {
+                    return;
+                }
+                int prev = i - 1, next = i + 1;
+                while (!FFTD.ContainsKey (prev) && prev >= 0)
+                {
+                    prev--;
+                }
+                if (prev == -1)
+                {
+                    return;
+                }
+                while (!FFTD.ContainsKey (next) && next < FFT.Length)
+                {
+                    next++;
+                }
+                if (next == FFT.Length)
+                {
+                    return;
+                }
+                var fftdPrev = FFTD [prev];
+                var step = FFTD [next] - fftdPrev;
+                var oneDiff = step / (next - prev);
+                FFT [i] = (float)(fftdPrev + oneDiff * (i - prev));
+            });
+            return FFT;
+        }
+#pragma warning disable S125
+        //private static double [] ApplyWindowFunction (double [] frame)
+        //{
+        //    switch (MainWindow.GetWindowFunction ())
+        //    {
+        //        case WindowFunction.Rectangular:
+        //            return frame;
+        //        case WindowFunction.Hamming:
+        //        {
+        //            _ = Parallel.For (0, frame.Length, (i) => frame [i] *= 0.54 + 0.46 * Math.Cos (Math.PI * i / frame.Length));
+        //            return frame;
+        //        }
+        //        case WindowFunction.Hann:
+        //        {
+        //            _ = Parallel.For (0, frame.Length, (i) => frame [i] *= (1 + Math.Cos (Math.PI * i / frame.Length)) / 2);
+        //            return frame;
+        //        }
+        //        default:
+        //            throw new NotImplementedException ();
+        //    }
+        //}
+#pragma warning restore S125 // Might be removed later
+    }
+}
---
+++ SoundAnalyser2/Soundfile.cs
@@ -0,0 +1,77 @@
+﻿using NAudio.Wave;
+using SoundAnalyser2.Parameters;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoundAnalyser2
+{
+    public class Soundfile
+    {
+        public int FrameLength { get; private set; }
+
+        private readonly float [] samples;
+
+        public float [] GetSamples () => samples;
+
+        public int SampleRate { get; }
+
+        private readonly string filename;
+        private float [] volume;
+
+        public Soundfile (string filename, int frameLength = 256)
+        {
+            using var wav = new WaveFileReader (filename);
+            if (wav.WaveFormat.SampleRate != 22050 || wav.WaveFormat.BitsPerSample != 16 || wav.WaveFormat.Channels != 1)
+            {
+                throw new FileFormatException (new Uri (filename), "Wav file has to have a sample rate of 22050, 16 bits per sample and mono channel.");
+            }
+            this.FrameLength = frameLength;
+            this.SampleRate = wav.WaveFormat.SampleRate;
+            this.filename = filename;
+            samples = (new float [wav.SampleCount]);
+            wav.ToSampleProvider ().Read (samples, 0, samples.Length);
+            RefreshCalculations ();
+        }
+
+        public void DrawSignalTimePlot (ref ScottPlot.WpfPlot plot)
+        {
+            if (plot is null)
+            {
+                throw new ArgumentNullException (nameof (plot));
+            }
+            plot.plt.Clear ();
+            plot.plt.Title (filename.Split ('\\').Last (), true);
+            plot.plt.XLabel ("sample");
+            plot.plt.PlotSignalConst (GetSamples ());
+            plot.Render ();
+        }
+
+        public void RefreshCalculations (int? frameLength = null)
+        {
+            if (frameLength.HasValue)
+            {
+                this.FrameLength = frameLength.Value;
+            }
+            var taskList = new Task<float []> []
+            {
+                Volume.Calculate (this)
+            };
+            Task.WaitAll (taskList);
+            volume = taskList [0].Result;
+        }
+
+        public void DrawVolumePlot (ref ScottPlot.WpfPlot plot)
+        {
+            if (plot is null)
+            {
+                throw new ArgumentNullException (nameof (plot));
+            }
+            plot.plt.Clear ();
+            plot.plt.Title ("Volume");
+            plot.plt.PlotSignalConst (volume);
+            plot.Render ();
+        }
+    }
+}
